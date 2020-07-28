15c9fab4c2ecfd9bd52f289d3cd28530c659f6aa
surajsnanavare <surajsnanavare@gmail.com>
2020-07-28 21:10:18 +0530

    whatsApp contact & contact form updates

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,7 +3,54 @@ $(window).load(function() {
     return false;
 });
 
+function send_contact_msg(){
+    var email =  $("#contact_email").val();
+    var name =  $("#contact_name").val();
+    var message =  $("#contact_msg").val();
+
+    var valid = '<p class="text-center">Please provide all required details!<p>'+
+                '<br><p class="text-center"><button type="button" class="btn btn-primary" data-dismiss="modal">Ok</button></p>';
+
+    var loading = '<div class="row">' +
+                    '<div class="col-md-12" id="contactStatusIcon">' +
+                        '<div class="loader" id="loader-3"></div>' +
+                    '</div>'+
+                    '<div class="col-md-12 contactStatusText" id="contactStatusText">'+
+                        '<p class="text-center">'+
+                            'Please wait!'+
+                        '</p>'+
+                    '</div>'+
+                '</div>';
+
+    var success = '<p class="text-center">Thank you! I will get back to you soon.<p>';
+
+    if(email=="" || name=="" || message==""){
+        $("#contactStatusBody").html(valid);    
+        $('#contactStatus').click();
+        return;
+    }
+
+    $("#contactStatusBody").html(loading);
+    $('#contactStatus').click();
+    $.post('https://submit-form.com/2CEudH3yeNJQ3h-rP8YKE', {
+      email: email,
+      name: name,
+      message: message
+    }).then(function (err) {
+        console.log(err);
+        $("#contactStatusBody").html(success);
+        $("#contact_email").val('');
+        $("#contact_name").val('');
+        $("#contact_msg").val('');
+        setTimeout(() => {
+            $("#contactStatusClose").click();
+            $("#contactStatusBody").html(loading);
+        }, 600 * 4);
+    });
+  }
+
 $(document).ready(function() {
+  
     $('#header_wrapper').scrollToFixed();
     $('.res-nav_click').click(function() {
         $('.main-nav').slideToggle();
